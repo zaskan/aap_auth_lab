1a6ef53d22bf56fc777cc1a06c68f2589606df60
root <root@podman.example.com>
2025-05-29 11:31:04 -0400

    Added local saml IdP

---
+++ config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+$config = [
+    'enable.saml20-idp' => true,
+    'memcache_store.prefix' => null,
+    'memcache_store.expires' => 36 * (60 * 60),
+    'metadata.sign.enable' => false,
+    'metadata.sign.privatekey' => null,
+    'metadata.sign.privatekey_pass' => null,
+    'metadata.sign.certificate' => null,
+    'proxy' => null,
+    'trusted.url.domains' => [],
+];
