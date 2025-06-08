cedf427355f57077e19be8285192c6fac15eb869
gitea <gitea@gitea.com>
2025-06-08 20:16:32 +0200

    Converting config

--- saml/config/config.php
+++ saml/config/config.php
@@ -1,7 +1,6 @@
 <?php
 
 $config = [
-    'auth.adminpassword' => '$2y$10$PnFsSEv.lda1Qlw4iMtmB.B.ab5y.aT56stBmo9hdCN.rUywQMChC',
     'enable.saml20-idp' => true,
     'memcache_store.prefix' => null,
     'memcache_store.expires' => 36 * (60 * 60),
