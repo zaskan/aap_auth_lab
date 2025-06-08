39aafc0790c7c3dc3206be8f4b989f1335c9a13d
root <root@podman.example.com>
2025-06-08 14:32:01 -0400

    Converting...

--- saml/config/config.php
+++ saml/config/config.php
@@ -1,6 +1,7 @@
 <?php
 
 $config = [
+    'auth.adminpassword' =>  '$2y$10$miYe.zDgbEKBio5rF21EuuVt3dFPquAN482To1uIo.3tW5k7zkSBG',
     'enable.saml20-idp' => true,
     'memcache_store.prefix' => null,
     'memcache_store.expires' => 36 * (60 * 60),
